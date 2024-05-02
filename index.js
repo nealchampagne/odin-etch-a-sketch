c32aa9a6154a7e93aa3eb4e24a18a3e20e3e7cec
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-05-02 14:43:33 -0700

    Add functionality to change color on mouseover
    Add the mouseover event listener and change color of the square to
    black. Additionally, if party mode is active, set it to a random color.

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 /** Initialize page elements */
 const canvas = document.createElement('div');
-const btn = document.createElement('button');
-canvas.setAttribute('id', 'container');
-btn.textContent = `Reset/Resize`;
+const buttons = document.createElement('div');
+const resetButton = document.createElement('button');
+const partyButton = document.createElement('button');
+const shadingButton = document.createElement('button');
 
-document.body.appendChild(btn);
+canvas.setAttribute('id', 'container');
+buttons.setAttribute('id', 'buttons');
+resetButton.textContent = `Reset/Resize`;
+partyButton.textContent = `Party Mode: Off`;
+shadingButton.textContent = `Shading Mode: Off`;
+
+document.body.appendChild(buttons);
+buttons.appendChild(resetButton);
+buttons.appendChild(partyButton);
+buttons.appendChild(shadingButton);
 document.body.appendChild(canvas);
 
+/** Mode switches */
+let partyMode = false;
+let shadingMode = false;
+
 /** Get user selection via button */
-btn.addEventListener('click', () => {
-  const input = prompt(`Enter 1-100 to choose your canvas size.`);
+resetButton.addEventListener('click', () => {
+  const input = prompt(`Enter 1-100 to choose your canvas resolution.`);
   if (Number.isInteger(Number(input)) === false) {
     alert(`Please enter an integer value.`);
   } else if (Number(input) < 1 || Number(input) > 100) {
@@ -32,10 +46,25 @@ const fillCanvas = sideNum => {
   for (i = 0; i < sideNum; i++) {
     const column = document.createElement('div');
     column.setAttribute('id', `column${i}`);
+    column.setAttribute('class', 'column');
     canvas.appendChild(column);
     for (j = 0; j < sideNum; j++) {
       const row = document.createElement('div');
       row.setAttribute('class', 'pixel');
+
+      /** if (shadingMode === true) {
+        row.style.backgroundColor = 'rgb(0,0,0,0)';
+      } */
+
+      row.addEventListener('mouseover', () => {
+        if (partyMode === true) {
+          row.style.backgroundColor = randomColor();
+        } else if (shadingMode === true) {
+          row.style.opacity += 0.1;
+        } else {
+          row.style.backgroundColor = 'black';
+        }
+      });
       document.getElementById(`column${i}`).appendChild(row);
     }
   }
@@ -46,4 +75,22 @@ document.addEventListener('DOMContentLoaded', () => {
   fillCanvas(16);
 });
 
-/** Color squares on hover */
+/** Random color generator */
+const randomColor = () => {
+  return `#${Math.floor((Math.random() * 16777215)).toString(16)}`;
+}
+
+/** Toggle party mode */
+partyButton.addEventListener('click', () => {
+  if (partyMode === false) {
+    partyMode = true;
+    shadingMode = false;
+    partyButton.textContent = `Party Mode: On`;
+    shadingButton.textContent = `Shading Mode: Off`;
+  } else {
+    partyMode = false;
+    partyButton.textContent = `Party Mode: Off`;
+  };
+  clearCanvas();
+  fillCanvas(16);
+});
